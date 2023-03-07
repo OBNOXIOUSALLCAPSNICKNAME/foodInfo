4ab55407a673ae6c310094ff823a421beea3e0e5
VladislavYglik <wtbfreenickname@gmail.com>
2023-03-07 19:38:43 +0300

    update
    Gradle reworked

---
+++ app/build.gradle.kts
@@ -0,0 +1,85 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("androidx.navigation.safeargs.kotlin")
+    id("kotlin-kapt")
+}
+
+kapt {
+    generateStubs = true
+}
+
+android {
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.foodinfo"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+    lint {
+        abortOnError = false
+        baseline = file("lint-baseline.xml")
+    }
+}
+
+dependencies {
+    implementation(Dependencies.RemoteData.gson)
+    implementation(Dependencies.RemoteData.gsonConverter)
+    implementation(Dependencies.RemoteData.retrofit)
+
+    kapt(Dependencies.DI.daggerCompiler)
+    implementation(Dependencies.DI.dagger)
+    implementation(Dependencies.DI.daggerAndroid)
+
+    implementation(Dependencies.MultiThreading.coroutines)
+    implementation(Dependencies.MultiThreading.coroutinesCore)
+
+    kapt(Dependencies.LocalData.roomCompiler)
+    implementation(Dependencies.LocalData.roomKTX)
+    implementation(Dependencies.LocalData.roomPaging)
+    implementation(Dependencies.LocalData.roomRuntime)
+
+    kapt(Dependencies.UI.glideCompiler)
+    implementation(Dependencies.UI.glide)
+    implementation(Dependencies.UI.paging)
+    implementation(Dependencies.UI.svg)
+    implementation(Dependencies.UI.core)
+    implementation(Dependencies.UI.appcompat)
+    implementation(Dependencies.UI.material)
+    implementation(Dependencies.UI.constraint)
+    implementation(Dependencies.UI.lifecycleViewModel)
+    implementation(Dependencies.UI.lifecycleRuntime)
+    implementation(Dependencies.UI.navigationUI)
+    implementation(Dependencies.UI.navigationFragment)
+
+    debugImplementation(Dependencies.Other.leakcanary)
+    implementation(Dependencies.Other.legacySupport)
+    testImplementation(Dependencies.Other.junit)
+    androidTestImplementation(Dependencies.Other.junitExt)
+    androidTestImplementation(Dependencies.Other.espresso)
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,38 @@
+object Versions {
+    object UI {
+        const val glide = "4.11.0"
+        const val svg = "1.4"
+        const val paging = "3.1.1"
+        const val core = "1.8.0"
+        const val appcompat = "1.3.0"
+        const val material = "1.4.0"
+        const val constraint = "2.0.4"
+        const val lifecycle = "2.5.0"
+        const val navigation = "2.5.0"
+    }
+
+    object LocalData {
+        const val room = "2.5.0"
+    }
+
+    object RemoteData {
+        const val retrofit = "2.9.0"
+        const val gson = "2.8.7"
+    }
+
+    object DI {
+        const val dagger = "2.42"
+    }
+
+    object MultiThreading {
+        const val coroutines = "1.6.3"
+    }
+
+    object Other {
+        const val legacySupport = "1.0.0"
+        const val junit = "4.13.2"
+        const val junitExt = "1.1.3"
+        const val espresso = "3.4.0"
+        const val leakcanary = "2.9.1"
+    }
+}
